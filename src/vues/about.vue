c1db511f272a5757c151eb451b25124ccab094b5
Gabriel Guzman <gabe10777@gmail.com>
2017-08-24 18:45:54 -0400

    first commit

---
+++ src/vues/about.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="about-wrapper">
+        <p>
+            {{msg}}
+        </p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'Hello world!'
+            }
+        }
+    }
+
+</script>
+
+<style>
+    .about-wrapper {
+        color: red;
+    }
+
+</style>
+
+<custom1>
+    This could be e.g. documentation for the component.
+</custom1>
